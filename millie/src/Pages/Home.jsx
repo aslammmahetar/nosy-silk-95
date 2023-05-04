97891184498e394a7c8d7f90dff943d7d7dbd8aa
unknown <lovkumar120169@gmail.com>
2023-05-04 22:28:35 +0530

    Day-1

--- millie/src/Pages/Home.jsx
+++ millie/src/Pages/Home.jsx
@@ -1,1 +1,17 @@
 //assign to lov
+import React from 'react'
+import { Last } from '../Conmponets/HomeComp/Last'
+import { Footer } from '../Conmponets/HomeComp/Footer'
+import { Brand } from '../Conmponets/HomeComp/Brand'
+import {Navbar} from "../Conmponets/HomeComp/Navbar"
+
+export const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Brand/>
+        <Last/>
+        <Footer/>
+    </div>
+  )
+}
