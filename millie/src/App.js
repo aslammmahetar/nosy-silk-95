8f497a53f8a8b46c68437c0f00b7810d69a5eb7f
aslammmahetar <mmahetaraslam@gmail.com>
2023-05-04 20:29:37 +0530

    day 1 commit

--- millie/src/App.js
+++ millie/src/App.js
@@ -1,24 +1,9 @@
 import './App.css';
-import Classes from './Pages/Classes';
-import { ChakraProvider } from '@chakra-ui/react';
-// import { app } from './FireBase/fireBase';
-// import { getDatabase } from 'firebase/database'
-import { BrowserRouter } from 'react-router-dom';
-
-// console.log(app)
-
-// const db = getDatabase(app)
-// console.log(db)
 
 function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <ChakraProvider>
-          <Classes />
-        </ChakraProvider>
-      </BrowserRouter>
     </div>
   );
 }
