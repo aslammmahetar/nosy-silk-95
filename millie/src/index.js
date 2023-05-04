fbd2e6defce6c8b6004cfae8da48713affca7682
sharadParadhi <112753856+sharadParadhi@users.noreply.github.com>
2023-05-04 22:17:21 +0530

    Update index.js

--- millie/src/index.js
+++ millie/src/index.js
@@ -7,9 +7,9 @@ import {ChakraProvider} from "@chakra-ui/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider>
+  
     <App />
-  </ChakraProvider>
+ 
     
   
 );
